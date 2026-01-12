69ff769120f2cd35cba3036b2f07c41540960e02
Sayyed Ashif <sayyedashif73@gmail.com>
2026-01-12 19:47:13 +0530

    Changes in server

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import app from './app.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const logger = createLogger('Server');
